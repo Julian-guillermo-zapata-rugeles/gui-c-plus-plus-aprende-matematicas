63151cf59238c50e58fe9540ffb38c29068f2e90
static <latanic@colossus.static>
2021-02-18 20:39:05 -0500

    se añadió al MotorMatemático la generación de numeros aleatorios

--- motormatematico.cpp
+++ motormatematico.cpp
@@ -2,5 +2,48 @@
 
 MotorMatematico::MotorMatematico()
 {
+    this->numero1=0;
+    this->numero2=0;
+    this->resultado=0;
+}
+
+
+
+unsigned short MotorMatematico::generarNumero(unsigned short nivel)
+{
+    unsigned short int numero;
+    srand(time(NULL));
+    numero=0+rand()%5+nivel;
+    return numero;
+
+}
+
+
+
+
+/*  ------------------ MÉTODOS SETTER Y GETTER --------------------   */
+
+unsigned short MotorMatematico::getNumero1() const
+{
+    return numero1;
+}
+
+unsigned short MotorMatematico::getNumero2() const
+{
+    return numero2;
+}
+
+unsigned short MotorMatematico::getResultado() const
+{
+    return resultado;
+}
+
+
+
+/*-------------------- OPERACIONES MATEMATICAS ---------------------------*/
+void MotorMatematico::generarOperacion(unsigned short nivel)
+{
+    this->numero1=generarNumero(nivel);
+    this->numero2=generarNumero(nivel);
 
 }
--- motormatematico.h
+++ motormatematico.h
@@ -2,11 +2,26 @@
 #define MOTORMATEMATICO_H
 
 #include <QObject>
-
+#include <QString>
+#include <random>
+#include <time.h>
 class MotorMatematico
 {
 public:
     MotorMatematico();
+    // metodos getter //
+    unsigned short getNumero1() const;
+    unsigned short getNumero2() const;
+    unsigned short getResultado() const;
+
+    void generarOperacion(unsigned short int nivel);
+    bool comprobarOperacion(unsigned short int eleccion);
+
+
+private:
+    unsigned short int numero1 , numero2 ;
+    unsigned short int generarNumero(unsigned short int nivel);
+
 };
 
 #endif // MOTORMATEMATICO_H
